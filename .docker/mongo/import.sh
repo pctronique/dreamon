bcf9552d660abc878863ea8e6d9fc95691f424b2
pctronique <pctronique@free.fr>
2023-09-23 11:32:53 +0200

    modif name variables

--- .docker/mongo/import.sh
+++ .docker/mongo/import.sh
@@ -4,5 +4,5 @@ do
    export $line
 done < ${0%/*}/.env
 for entry in `ls *.json`; do
-    mongoimport -u=$MONGO_ROOT_USERNAME -p=$MONGO_ROOT_PASSWORD -h=mongo --authenticationDatabase=admin --db $MONGO_DATABASE --type json --file /mongo-seed/$entry --jsonArray
+    mongoimport -u=$SGBD_ROOT_USERNAME -p=$SGBD_ROOT_PASSWORD -h=mongo --authenticationDatabase=admin --db $SGBD_DATABASE --type json --file /mongo-seed/$entry --jsonArray
 done
